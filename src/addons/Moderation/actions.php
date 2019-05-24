331f871bed571b7525908cbb22ba06ec7670a0a0
Mirko <mirko.friedrich@blandland.de>
2019-05-24 02:28:23 +0200

    add moderation

---
+++ src/addons/Moderation/actions.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @file
+ * Implements moderation related actions on cockpit collections.
+ */
+
+/**
+ * Exclude unpublished entries.
+ */
+$app->on('collections.find.before', function ($name, &$options) use ($app) {
+
+  // Get the collection.
+  $collection = $this->module('collections')->collection($name);
+
+  // Exclude on unpublished state.
+  foreach ($collection['fields'] as $field) {
+    if ($field['type'] === 'moderation') {
+      $options['filter']['$and'][] = ["{$field['name']}" => ['$exists' => TRUE]];
+      $options['filter']['$and'][] = ["{$field['name']}" => ['$ne' => 'Unpublished']];
+      break;
+    }
+  }
+  // Extend filters to other addons.
+  $app->trigger("moderation.find.before", [$name, &$options]);
+});
+
+/**
+ * Iterate over the collection entries.
+ *
+ * For the draft ones check if we have a previous published revision.
+ */
+$app->on('collections.find.after', function ($name, &$entries) use ($app) {
+  $token = $app->param('previewToken', FALSE);
+  // If we have a valid previewToken don't need to go further.
+  if ($token && $app->module('moderation')->validateToken($token)) {
+    return;
+  }
+
+  // Get the moderation field.
+  $moderation_field = $app->module('moderation')->getModerationField($name);
+  if (!$moderation_field) {
+    return;
+  }
+
+  foreach ($entries as $idx => $entry) {
+    if (!isset($entry[$moderation_field])) {
+      continue;
+    }
+
+    // If Draft ensure we retrieve the latest published revision.
+    if ($entry[$moderation_field] == 'Draft') {
+      $revisions = $app->helper('revisions')->getList($entry['_id']);
+      $published = $app->module('moderation')->getLastPublished($entry['_id'], $moderation_field, $revisions);
+      if ($published) {
+        $published = [$published];
+        $populated = cockpit_populate_collection($published, 1);
+        $published = current($published);
+        $entries[$idx] = $published;
+      }
+      else {
+        unset($entries[$idx]);
+      }
+    }
+
+    // Remove moderation field from the answer.
+    if (isset($entries[$idx][$moderation_field])) {
+      unset($entries[$idx][$moderation_field]);
+    }
+  }
+  // Rebuild array indices.
+  $entries = array_values($entries);
+});
---
+++ src/addons/Moderation/views/partials/entry-aside.php
@@ -0,0 +1,46 @@
+<div class="uk-margin moderation-status" if="{field && field.length && moderation_field}">
+  <div class="uk-width-1-1 uk-form-select uk-moderation-element uk-moderation-{ entry[moderation_field] }">
+    <label class="uk-text-small">@lang('Moderation Status:')</label>
+    <div class="uk-margin-small-top">
+      <span class="uk-badge uk-badge-outline">
+        <i if="{entry[moderation_field] == 'Unpublished'}" class="uk-icon-circle-o"></i>
+        <i if="{entry[moderation_field] == 'Draft'}" class="uk-icon-pencil"></i>
+        <i if="{entry[moderation_field] == 'Published'}" class="uk-icon-circle"></i>
+        @lang("{entry[moderation_field]}")
+      </span>
+    </div>
+    <select bind="entry.{moderation_field}">
+      <option selected="{ entry[moderation_field] === 'Unpublished' }" value="Unpublished">@lang('Unpublished')</option>
+      <option selected="{ entry[moderation_field] === 'Draft' }" value="Draft">@lang('Draft')</option>
+      <option selected="{ entry[moderation_field] === 'Published' }" value="Published">@lang('Published')</option>
+    </select>
+  </div>
+</div>
+
+<script>
+  var $this = this;
+  $this.moderation_field = 'status';
+
+  this.on('mount', function() {
+
+    $this.field = this.collection.fields.filter(function(definition) {
+      return definition.type === 'moderation';
+    });
+
+    if (!$this.field.length || $this.field[0].name === undefined) {
+      return;
+    }
+
+    $this.moderation_field = $this.field[0].name;
+
+    $this.entry[$this.moderation_field] = $this.entry[$this.moderation_field] || 'Draft';
+
+    window.setTimeout(function() {
+      sidebar = document.querySelector('.uk-width-medium-1-4.uk-flex-order-first');
+      sidebar.insertBefore(document.querySelector('.moderation-status'), sidebar.childNodes[0]);
+    }, 50);
+
+    $this.update();
+  });
+
+</script>
